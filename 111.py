a33ff14b1024c8092062e32a963e7e9705ab898d
Maxim27 <89885577000@mail.ru>
2025-06-21 16:19:14 +0300

    commit 1

---
+++ 111.py
@@ -0,0 +1,18 @@
+t = int(input())
+for i in range(t):
+    n, x = map(int, input().split())
+    a = [int(i) for i in input().split()]
+    if a.count(1) == 0:
+        print('YES')
+
+    else:
+
+        ind1 = a.index(1)
+        ind2 = n - 1 - a[::-1].index(1)
+        mx = ind2 - ind1 + 1
+
+        if x >= mx:
+            print('YES')
+        else:
+            print('NO')
+
